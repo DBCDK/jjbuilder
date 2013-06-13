9045ad853898f82e029da735c5e90c2b657b488b
Michael Carlsson <mca@dbc.dk>
2013-06-13 14:18:29 +0200

    Add description.

--- jjbuilder.py
+++ jjbuilder.py
@@ -5,6 +5,10 @@ from simpletal import simpleTAL, simpleTALES
 
 def generate_job(data):
   context = simpleTALES.Context()
+
+  for key in data.keys():
+    context.addGlobal(key, data[key])
+
   template_file = open('templates/config.xml', 'rt')
   template = simpleTAL.compileXMLTemplate(template_file)
   template_file.close()
--- test/test_jjbuilder.py
+++ test/test_jjbuilder.py
@@ -2,8 +2,14 @@ from jjbuilder import generate_job
 import pytest
 
 
-def testGenerateJob():
-  lines = file('test/jobs/empty.xml').readlines()
+def assertJob(data, result_file):
+  lines = file(result_file).readlines()
   whole_file = reduce(str.__add__, lines)
-  job = generate_job({})
+  job = generate_job(data)
   assert job == whole_file
+
+def testGenerateEmptyJob():
+  assertJob({}, 'test/jobs/empty.xml')
+
+def testGenerateDescribedJob():
+  assertJob({'description': 'Some description of the job.'}, 'test/jobs/description.xml')
